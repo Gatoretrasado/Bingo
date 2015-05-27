6cfbed0012872a0d0e6c5be75f434d8d9f621407
Gatoretrasado <josan1995@msn.com>
2015-05-27 22:16:11 +0200

    lineas ya

--- src/bingo/Bingo.java
+++ src/bingo/Bingo.java
@@ -1,25 +1,28 @@
-
 package bingo;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-
-public class Bingo extends JFrame{
+public class Bingo extends JFrame {
 
     JPanel panel_principal;
     JPanel panel_botones;
-    
-    public Bingo(){
+    ArrayList<Carton> cartones = new ArrayList<>();
+    ArrayList<Integer> numerosNombrados = new ArrayList<>();
+    int numeroMagico = 0;
+
+    public Bingo() {
         //TITULO DE LA VENTANA
         setTitle("Bingo");
         //TAMAÑO DE LA VENTANA
-        setSize(300,300);
+        setSize(300, 300);
         //SI NOS DEJA CAMBIAR SUS DIMENSIONES O NO 
         setResizable(true);
         //POSICION EN LA QUE APARECE
@@ -31,17 +34,17 @@ public class Bingo extends JFrame{
         //HACEMOS VISIBLE LA VENTANA
         setVisible(true);
     }
-    
-    void visible(){
+
+    void visible() {
         panel_principal = new JPanel();
         panel_principal.setLayout(new BorderLayout());
         panel_botones = new JPanel();
-        panel_botones.setLayout(new GridLayout(3,1,8,8));
+        panel_botones.setLayout(new GridLayout(3, 1, 8, 8));
         crearBotones();
-        panel_principal.add("Center",panel_botones);
+        panel_principal.add("Center", panel_botones);
         getContentPane().add(panel_principal);
     }
-    
+
     public static void main(String[] args) {
         Bingo b = new Bingo();
     }
@@ -49,12 +52,33 @@ public class Bingo extends JFrame{
     void crearBotones() {
         JButton btn_iniciar = new JButton();
         btn_iniciar.setText("Iniciar");
+        btn_iniciar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                try {
+                    btn_iniciar.setText("Jugar");
+                    jugar();
+                } catch (Exception err) {
+                    System.out.println("Error: " + err);
+                }
+            }
+        });
         panel_botones.add(btn_iniciar);
-        
+
         JButton btn_carton = new JButton();
         btn_carton.setText("Carton");
+        btn_carton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                try {
+                    cartones.add(new Carton());
+                } catch (Exception err) {
+                    System.out.println("Error: " + err);
+                }
+            }
+        });
         panel_botones.add(btn_carton);
-        
+
         JButton btn_salir = new JButton();
         btn_salir.setText("Salir");
         panel_botones.add(btn_salir);
@@ -62,13 +86,30 @@ public class Bingo extends JFrame{
             @Override
             public void actionPerformed(ActionEvent evt) {
                 try {
-                   
-                    
+
                 } catch (Exception err) {
                     System.out.println("Error: " + err);
                 }
             }
         });
     }
-    
+
+    public void jugar() {
+        boolean esta = false;
+
+        do {
+            numeroMagico = ((int) (Math.random() * 90));
+            if (numerosNombrados.contains(numeroMagico) == false && numeroMagico != 0) {
+                numerosNombrados.add(numeroMagico);
+                System.out.println("numero: " + numeroMagico);
+                esta = true;
+            }
+        } while (!esta);
+
+        for (Carton nCarton : cartones) {
+            nCarton.buscarNumero(numeroMagico);
+            
+        }
+    }
+
 }
--- src/bingo/Carton.java
+++ src/bingo/Carton.java
@@ -2,11 +2,16 @@ package bingo;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import static java.awt.Color.BLACK;
+import static java.awt.Color.red;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Collections;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Carton extends JFrame {
@@ -16,8 +21,11 @@ public class Carton extends JFrame {
     JButton boton[];
     int espacios[][] = new int[3][9];
     int compColumna[] = new int[9];
-    int contL = 0;
-    int htotales;
+    int contL = 0, contIntro = 0;
+    //cuenta huecos y numeros totales
+    int htotales, contNumeros = 0, contadorPosicion = 0;
+    ArrayList<Integer> numeros = new ArrayList<Integer>();
+    int i = 0;
 
     public Carton() {
         //TITULO DE LA VENTANA
@@ -49,10 +57,10 @@ public class Carton extends JFrame {
 
     void crearBotones() {
         boton = new JButton[27];
+        i = 0;
 
         superMetodoCrearCartones();
-        int i = 0;
-        int n = (int) (Math.random() * 10);
+
         for (int col = 0; col < 3; col++) {
             for (int f = 0; f < 9; f++) {
                 boton[i] = new JButton("" + espacios[col][f]);
@@ -65,10 +73,12 @@ public class Carton extends JFrame {
                     @Override
                     public void actionPerformed(ActionEvent evt) {
                         try {
-                            System.out.println("Pulsado");
 
+                            cambioColor(boton[i - 1]);
+                            System.out.println("hola");
                         } catch (Exception err) {
                             System.out.println("Error: " + err);
+                            System.out.println("aqui");
                         }
                     }
                 });
@@ -77,18 +87,32 @@ public class Carton extends JFrame {
         }
     }
 
+    void cambioColor(JButton btn) {
+        btn.setBackground(Color.blue);
+    }
+
     void superMetodoCrearCartones() {
         do {
             //reset
             htotales = 0;
             contL = 0;
+            contadorPosicion = 0;
+            contIntro = 0;
             for (int f = 0; f < 9; f++) {
                 compColumna[f] = 0;
             }
 
-            for (int col = 0; col < 3; col++) {
-                for (int f = 0; f < 9; f++) {
-                    espacios[col][f] = 1;
+            for (contadorPosicion = 0; contadorPosicion < 27; contadorPosicion++) {
+                rellenarTablaNumeros();
+            }
+            Collections.sort(numeros);
+
+            for (int f = 0; f < 9; f++) {
+                for (int col = 0; col < 3; col++) {
+                    espacios[col][f] = numeros.get(contIntro);
+                    //System.out.print(contIntro+" ->  ");
+                    //System.out.println(numeros.get(contIntro));
+                    contIntro++;
                 }
             }
 
@@ -109,7 +133,149 @@ public class Carton extends JFrame {
     }
 
     void rellenarTablaNumeros() {
+        int preNumero;
+        switch (numeros.size()) {
+            case 0:
+            case 1:
+            case 2:
+                preNumero = ((int) (Math.random() * 10) + 1);
+                if (preNumero != 10 && numeros.contains(preNumero) == false) {
+                    numeros.add(preNumero);
+                    contNumeros++;
+                } else {
+                    contadorPosicion--;
+                }
+                break;
+            case 3:
+            case 4:
+            case 5:
+                preNumero = ((int) (Math.random() * 10) + 10);
+                if (preNumero != 20 && numeros.contains(preNumero) == false) {
+                    numeros.add(preNumero);
+                    contNumeros++;
+                } else {
+                    contadorPosicion--;
+                }
+                break;
+            case 6:
+            case 7:
+            case 8:
+                preNumero = ((int) (Math.random() * 10) + 20);
+                if (preNumero != 30 && numeros.contains(preNumero) == false) {
+                    numeros.add(preNumero);
+                    contNumeros++;
+                } else {
+                    contadorPosicion--;
+                }
+                break;
+            case 9:
+            case 10:
+            case 11:
+                preNumero = ((int) (Math.random() * 10) + 30);
+                if (preNumero != 40 && numeros.contains(preNumero) == false) {
+                    numeros.add(preNumero);
+                    contNumeros++;
+                } else {
+                    contadorPosicion--;
+                }
+                break;
+            case 12:
+            case 13:
+            case 14:
+                preNumero = ((int) (Math.random() * 10) + 40);
+                if (preNumero != 40 && numeros.contains(preNumero) == false) {
+                    numeros.add(preNumero);
+                    contNumeros++;
+                } else {
+                    contadorPosicion--;
+                }
+                break;
+            case 15:
+            case 16:
+            case 17:
+                preNumero = ((int) (Math.random() * 10) + 50);
+                if (preNumero != 50 && numeros.contains(preNumero) == false) {
+                    numeros.add(preNumero);
+                    contNumeros++;
+                } else {
+                    contadorPosicion--;
+                }
+                break;
+            case 18:
+            case 19:
+            case 20:
+                preNumero = ((int) (Math.random() * 10) + 60);
+                if (preNumero != 60 && numeros.contains(preNumero) == false) {
+                    numeros.add(preNumero);
+                    contNumeros++;
+                } else {
+                    contadorPosicion--;
+                }
+                break;
+            case 21:
+            case 22:
+            case 23:
+                preNumero = ((int) (Math.random() * 10) + 70);
+                if (preNumero != 70 && numeros.contains(preNumero) == false) {
+                    numeros.add(preNumero);
+                    contNumeros++;
+                } else {
+                    contadorPosicion--;
+                }
+                break;
+            case 24:
+            case 25:
+            case 26:
+                preNumero = ((int) (Math.random() * 10) + 80);
+                if (numeros.contains(preNumero) == false) {
+                    numeros.add(preNumero);
+                    contNumeros++;
+                } else {
+                    contadorPosicion--;
+                }
+                break;
 
+        }
+
+    }
+
+    static public boolean cantadoLinea = false, linea = false;
+
+    public void buscarNumero(int numeroMagico) {
+        int nboton = 0;
+
+        for (int fila = 0; fila < 3; fila++) {
+            for (int columna = 0; columna < 9; columna++) {
+
+                if (espacios[fila][columna] == numeroMagico) {
+                    boton[nboton].setBackground(Color.red);
+                    if (cantadoLinea == false) {
+                        compLinea();
+                    }
+                }
+                nboton++;
+            }
+        }
+    }
+
+    public void compLinea() {
+        int nboton = 0, correcto;
+        for (int fila = 0; fila < 3; fila++) {
+            correcto = 0;
+            for (int columna = 0; columna < 9; columna++) {
+                if (boton[nboton].getBackground().equals(red) || boton[nboton].getBackground().equals(BLACK)) {
+                    correcto++;
+                    
+                }
+                nboton++;
+            }
+            if (correcto == 9) {
+                linea = true;
+                cantadoLinea = true;
+                JOptionPane.showMessageDialog(null, "Linea Realizada", "LINEA", JOptionPane.INFORMATION_MESSAGE);
+                System.out.println("linea");
+            }
+        }
     }
 
     public static void main(String[] args) {
